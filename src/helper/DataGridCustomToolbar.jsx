0abe0d39e73f032e4bfaefad9cc2d43eca0b0f5d
shahbaz-alam7 <alamshahbaz287@gmail.com>
2023-01-13 02:42:06 +0530

    Geography and transaction page

---
+++ src/helper/DataGridCustomToolbar.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Search } from "@mui/icons-material";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarExport,
+  GridToolbarDensitySelector,
+} from "@mui/x-data-grid";
+import FlexBetween from "./FlexBetween";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+const DataGridCustomToolbar = ({ searchInput, setSearchInput, setSearch }) => {
+  return (
+    <GridToolbarContainer>
+      <FlexBetween width="100%">
+        <FlexBetween>
+          <GridToolbarColumnsButton />
+          <GridToolbarDensitySelector />
+          <GridToolbarExport />
+        </FlexBetween>
+        <TextField
+          label="Search..."
+          sx={{ mb: "0.5rem", width: "15rem" }}
+          onChange={(e) => setSearchInput(e.target.value)}
+          value={searchInput}
+          variant="standard"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => {
+                    setSearch(searchInput);
+                    setSearchInput("");
+                  }}
+                >
+                  <Search />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </FlexBetween>
+    </GridToolbarContainer>
+  );
+};
+
+export default DataGridCustomToolbar;
